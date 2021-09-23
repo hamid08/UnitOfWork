84abf83fe95135830dca6a7a0f4f2718d8861d27
hamid08 <46776527+hamid08@users.noreply.github.com>
2021-09-23 08:15:17 +0330

    Add files via upload

---
+++ IDriverRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RohaniShop.ViewModels.Driver;
+
+namespace RohaniShop.Data.Contracts
+{
+   public interface IDriverRepository
+    {
+        Task<List<DriversViewModel>> GetPaginateDriversAsync(int offset, int limit, string orderBy,
+            string searchText);
+
+        Task<List<DriversViewModel>> FinAllDriversAsync();
+    }
+}
---
+++ IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RohaniShop.Data.Contracts
+{
+    public interface IUnitOfWork
+    {
+        IBaseRepository<TEntity> BaseRepository<TEntity>() where TEntity : class;
+        ICustomerRepository CustomerRepository { get; }
+        IDriverRepository DriverRepository { get; }
+        IProductRepository ProductRepository { get; }
+        IFactorRepository FactorRepository { get; }
+        IFactorDriverRepository FactorDriverRepository { get; }
+        RohaniShopDBContext _Context { get; }
+        Task Commit();
+    }
+}
