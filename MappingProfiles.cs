4d5674194b2fc5f9c3ac1de97f5887c8b0744fbf
hamid08 <46776527+hamid08@users.noreply.github.com>
2021-09-23 08:13:12 +0330

    Add files via upload

---
+++ MappingProfiles.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using RohaniShop.Entities;
+using RohaniShop.Entities.identity;
+using RohaniShop.ViewModels.Manage;
+using RohaniShop.ViewModels.RoleManager;
+using RohaniShop.ViewModels.UserManager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RohaniShop.ViewModels;
+using RohaniShop.ViewModels.Customer;
+using RohaniShop.ViewModels.Driver;
+using RohaniShop.ViewModels.Factor;
+using RohaniShop.ViewModels.FactorProduct;
+using RohaniShop.ViewModels.Product;
+
+namespace RohaniShop.IocConfig.AutoMapper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Role, RolesViewModel>().ReverseMap()
+                    .ForMember(p => p.Users, opt => opt.Ignore())
+                    .ForMember(p => p.Claims, opt => opt.Ignore());
+
+            
+
+            CreateMap<User, UsersViewModel>().ReverseMap()
+                  .ForMember(p => p.Claims, opt => opt.Ignore());
+
+            CreateMap<User, ProfileViewModel>().ReverseMap()
+                   .ForMember(p => p.Claims, opt => opt.Ignore());
+          
+            CreateMap<Customer, CustomersViewModel>().ReverseMap();
+            CreateMap<Driver, DriversViewModel>().ReverseMap();
+            CreateMap<Product, ProductsViewModel>().ReverseMap();
+            CreateMap<Factor, FactorsViewModel>().ReverseMap();
+            CreateMap<FactorProduct, FactorProductsViewModel>().ReverseMap();
+
+
+
+        }
+    }
+}
